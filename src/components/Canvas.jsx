1daf4f4ef640188a5a9f7e9ebb3c94a77e7b0da5
Muhammad imran <muhammadimran27584@gmail.com>
2025-09-11 22:41:08 +0500

    data file added

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,40 +1,43 @@
 import React, { use } from 'react';
-import { useEffect, useRef ,useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import images from '../utili/images.js';
 import { gsap } from 'gsap';
 import { useGSAP } from "@gsap/react";
 function Canvas() {
   const canvasRef = useRef();
-  const [index, setIndex] = useState({value:0});
+  const [index, setIndex] = useState({ value: 0 });
 
-useGSAP(() => {
+  useGSAP(() => {
 
-  gsap.to(index, {
-    value: 149,
-    duration: 3,
-    ease: "linear",
-    repeat: -1,
-    onUpdate: () => {
-      setIndex({ value: Math.round(index.value) });
-    }
+    gsap.to(index, {
+      value: 149,
+      duration: 3,
+      ease: "linear",
+      repeat: -1,
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      }
+    });
   });
-});
 
-useEffect(() => {
-  const canvas = canvasRef.current;
-  const context = canvas.getContext('2d');
-  const img = new Image();
-  img.src = images[index.value];
-  img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-   
-    context.drawImage(img, 0, 0);
-  };
-}, [index]);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    img.src = images[index.value];
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      context.drawImage(img, 0, 0);
+    };
+  }, [index]);
 
   return (
-    <canvas id='canvas' ref={canvasRef} className='w-[18rem] h-[18rem]' ></canvas>
+    <canvas id='canvas' 
+            ref={canvasRef} 
+            className='absulute' 
+            ></canvas>
   )
 }
 
